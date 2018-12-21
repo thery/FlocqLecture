93c778199b230ec9651253e80423771a9adf1559
thery <Laurent.Thery@inria.fr>
2018-12-21 15:54:23 +0100

    lecture 4

---
+++ lecture4.v
@@ -0,0 +1,224 @@
+(******************************************************************************)
+(*                                                                            *)
+(*      LECTURE :  Floating-point numbers and formal proof                    *)
+(*                       Laurent.Thery@inria.fr       12/06/2016              *)
+(*                                                                            *)
+(******************************************************************************)
+
+(*                                                                              
+  we present the notion of ulp and the concrete IEEE 754 format                 
+*)
+
+Require Import Psatz.
+From Flocq Require Import FTZ Core Operations Binary Bits.
+
+Open Scope R_scope.
+
+Section Lecture4.
+
+(* Variables for our examples *)
+Variable vx vy : R.
+(* Basis *)
+Variable r : radix.
+(* Translating funtion for the exponent *)
+Variable phi : Z -> Z.
+Hypothesis vPhi : Valid_exp phi.
+(* Rounding function *)
+Variable rnd : R -> Z.
+Hypothesis vRound : Valid_rnd rnd.
+(* Choice function *)
+Variable choice : Z -> bool.
+
+Check ulp r phi vx.
+
+Eval lazy beta delta [ulp] in ulp r phi vx.
+Eval lazy beta delta [ulp cexp] in ulp r phi vx.
+
+Check ulp_neq_0.
+
+(*                                                                              
+  Prove that if phi is monotone so is ulp                                       
+                                                                                
+Fact ex1 x y :                                                                  
+  x <> 0 -> Monotone_exp phi -> Rabs x <= Rabs y -> ulp r phi x <= ulp r phi y. 
+                                                                                
+Hints:                                                                          
+                                                                                
+Check mag_le_abs.                                                           
+Check bpow_le.                                                                  
+                                                                                
+*)
+
+Check round_UP_DN_ulp.
+
+Check round_UP_DN_ulp r phi vx.
+
+(*                                                                              
+  Reprove round_DN_UP_ulp                                                       
+                                                                                
+Fact ext2 x :                                                                   
+  ~ generic_format r phi x ->                                                   
+  round r phi Zceil x = round r phi Zfloor x + ulp r phi x.                     
+                                                                                
+Hints:                                                                          
+                                                                                
+Check scaled_mantissa_mult_bpow.                                                
+Check Zceil_floor_neq.                                                          
+Check Ztrunc_Z2R.                                                               
+                                                                                
+*)
+
+Check error_lt_ulp.
+
+(*                                                                              
+  Reprove error_lt_ulp :                                                        
+  Fact ext3 x : x <> 0 -> Rabs (round r phi rnd x - x) < ulp r phi x.           
+                                                                                
+Hints :                                                                         
+                                                                                
+Check round_DN_UP_lt.                                                           
+Check round_DN_or_UP.                                                           
+Check round_UP_DN_ulp.                                                          
+                                                                                
+*)
+
+Check error_le_half_ulp.
+
+(*                                                                              
+  Reprove error_le_half_ulp :                                                   
+  Fact ext4 x :                                                                 
+    Rabs (round r phi (Znearest choice) x - x) <= / 2 * ulp r phi x.            
+                                                                                
+Hints:                                                                          
+                                                                                
+Check round_N_pt.                                                               
+Check generic_format_round.                                                     
+Check round_DN_UP_lt.                                                           
+Check round_DN_or_UP.                                                           
+Check round_UP_DN_ulp.                                                          
+                                                                                
+*)
+
+(* The IEEE 754 norm                                                          *)
+
+(* Simple precision *)
+Check binary32.
+(* Addition *)
+Check b32_plus.
+(* Rounding mode *)
+Print mode.
+
+(* Double precision *)
+Check binary64.
+(* Addition *)
+Check b64_plus.
+
+(* Two instances of a generic construct *)
+Print binary32.
+Print binary64.
+Check binary_float.
+
+
+(* Generic float                              *)
+(* Variables for the precision and the exponent range *)
+Variable vp ve : Z.
+
+(* A float : a sign, a mantissa, an exponent *)
+Variable s : bool.
+Variable m : positive.
+Variable e : Z.
+Variable H : bounded vp ve m e = true.
+
+
+Let f := B754_finite vp ve s m e H.
+Check f.
+Check is_finite vp ve.
+Compute is_finite vp ve f.
+
+(* Checking bound *)
+Eval lazy beta zeta iota delta [bounded] in 
+  (bounded vp ve m e).
+
+(* Test on the mantissa *)
+Check  canonical_canonical_mantissa vp ve s m e.
+Print canonical.
+Print cexp.
+
+(* Infinity *)
+
+(* + inf *)
+
+Let p_inf := B754_infinity vp ve false.
+Check p_inf.
+
+(* - inf *)
+
+Let n_inf := B754_infinity vp ve true.
+Check n_inf.
+
+(* Zero *)
+
+(* + zero *)
+
+Let p_z := B754_zero vp ve false.
+Check p_z.
+
+(* - zero *)
+
+Let n_z := B754_zero vp ve false.
+Check n_z.
+
+(* Nan *)
+
+
+(* Pay load for Nan *)
+
+Variable pl : positive.
+Check nan_pl vp pl.
+Eval lazy beta delta [nan_pl] in nan_pl vp pl.
+Variable plT : nan_pl vp pl = true.
+
+Let na := B754_nan vp ve s pl plT.
+
+Check is_nan vp ve.
+Compute is_nan vp ve.
+
+(* From binary to R *)
+
+Check B2R vp ve f.
+Eval lazy beta iota zeta delta [B2R f] in B2R vp ve f.
+Eval lazy beta iota zeta delta [B2R f cond_Zopp] in B2R vp ve f.
+
+(* Opposite *)
+
+Check Bopp vp ve.
+Eval lazy beta delta [Bopp] in Bopp vp ve.
+
+(* Instantiation for single precision *)
+Print b32_opp.
+
+(* Involutive *)
+Check Bopp_involutive vp ve.
+
+(* Value *)
+Check B2R_Bopp vp ve.
+
+(* Translating rounding modes *)
+
+Check round_mode.
+Print round_mode.
+
+(* Addition *)
+Check Bplus vp ve.
+Eval lazy beta delta [Bplus] in Bplus vp ve.
+
+(* Instantiation for single precision *)
+Print b32_plus.
+Print  binop_nan_pl32.
+
+SearchAbout Bplus.
+
+(* Involutive *)
+Check Bplus_correct vp ve.
+
+End Lecture4.
---
+++ lecture4_solution.v
@@ -0,0 +1,150 @@
+(******************************************************************************)
+(*                                                                            *)
+(*      LECTURE :  Floating-point numbers and formal proof                    *)
+(*                       Laurent.Thery@inria.fr       12/04/2013              *)
+(*                                                                            *)
+(******************************************************************************)
+
+(* Solutions of lecture 4 *)
+
+Require Import Psatz.
+From Flocq Require Import FTZ Core Operations.
+
+Open Scope R_scope.
+
+Section Solution4.
+
+Variable r : radix.
+Variable phi : Z -> Z.
+Hypothesis vPhi : Valid_exp phi.
+Variable rnd : R -> Z.
+Hypothesis vRound : Valid_rnd rnd.
+Variable choice : Z -> bool.
+
+(*                                                                              
+  Prove that if phi is monotone so is ulp                                       
+*)
+
+Fact ex1 x y :
+  x <> 0 -> Monotone_exp phi -> 
+   Rabs x <= Rabs y -> ulp r phi x <= ulp r phi y.
+Proof.
+intros NZx Mphi Rxy.
+assert (NZy : y <> 0) by (split_Rabs; lra).
+rewrite !ulp_neq_0; auto.
+apply bpow_le.
+apply Mphi.
+apply mag_le_abs; assumption.
+Qed.
+
+(*                                                                              
+Hints:                                                                          
+                                                                                
+Check ln_beta_le_abs.                                                           
+Check bpow_le.                                                                  
+                                                                                
+*)
+
+(*                                                                              
+  Reprove ulp_DN_UP                                                             
+*)
+
+Fact ext2 x : 
+  ~ generic_format r phi x ->
+  round r phi Zceil x = round r phi Zfloor x + ulp r phi x.
+Proof.
+intros H.
+assert (NZx : x <> 0).
+  contradict H; rewrite H; apply generic_format_0.
+rewrite ulp_neq_0; auto.
+unfold round, F2R; simpl.
+rewrite Zceil_floor_neq; rewrite ?plus_IZR; simpl; try ring.
+contradict H.
+unfold generic_format, F2R; simpl.
+rewrite <- H, Ztrunc_IZR, H.
+rewrite scaled_mantissa_mult_bpow; auto.
+Qed.
+
+(*                                                                              
+Hints:                                                                          
+                                                                                
+Check scaled_mantissa_mult_bpow.                                                
+Check Zceil_floor_neq.                                                          
+Check Ztrunc_Z2R.                                                               
+                                                                                
+*)
+
+(*                                                                              
+  Reprove error_lt_ulp                                                          
+*)
+
+
+Fact ext3 x : x <> 0 -> Rabs (round r phi rnd x - x) < ulp r phi x.
+Proof.
+intro NZx.
+assert (H : generic_format r phi x \/ ~ generic_format r phi x).
+  unfold generic_format, F2R; simpl.
+  set (y := _ * _); case (Req_dec x y); auto.
+destruct H.
+  set (y := _ - _); replace y with 0.
+    rewrite Rabs_R0, ulp_neq_0; auto; apply bpow_gt_0.
+  unfold y; rewrite round_generic; auto; ring.
+assert (H1 := round_DN_UP_lt _ _ _ H).
+assert (H2 := round_UP_DN_ulp _ _ _ H).
+destruct (round_DN_or_UP r phi rnd x).
+split_Rabs; lra.
+split_Rabs; lra.
+Qed.
+
+(*                                                                              
+Hints :                                                                         
+                                                                                
+Check round_DN_UP_lt.                                                           
+Check round_DN_or_UP.                                                           
+Check ulp_DN_UP.                                                                
+                                                                                
+*)
+
+(*                                                                              
+  Reprove error_le_half_ulp                                                     
+*)
+
+Search ulp.
+Fact ext4 x :
+    Rabs (round r phi (Znearest choice) x - x) <=
+         / 2 * ulp r phi x.
+Proof.
+assert (H : generic_format r phi x \/ ~ generic_format r phi x).
+  unfold generic_format, F2R; simpl.
+  set (y := _ * _); case (Req_dec x y); auto.
+destruct H.
+  set (y := _ - _); replace y with 0.
+    rewrite Rabs_R0.
+    apply Rmult_le_pos; try lra; apply ulp_ge_0.
+  unfold y; rewrite round_generic; auto; try ring.
+  apply valid_rnd_N.
+destruct (round_N_pt r phi choice x) as (Hr1, Hr2).
+assert (H1 := generic_format_round r phi Zfloor x).
+assert (H2 := generic_format_round r phi Zceil x).
+assert (H3 := Hr2 _ H1).
+assert (H4 := Hr2 _ H2).
+assert (H5 := round_UP_DN_ulp _ _ _ H).
+assert (H6 := round_DN_UP_lt _ _ _ H).
+destruct (round_DN_or_UP r phi (Znearest choice) x).
+revert H3 H4.
+split_Rabs; try lra.
+split_Rabs; try lra.
+Qed.
+
+(*                                                                              
+Hints:                                                                          
+                                                                                
+Check round_N_pt.                                                               
+Check generic_format_round.                                                     
+Check round_DN_UP_lt.                                                           
+Check round_DN_or_UP.                                                           
+Check ulp_DN_UP.                                                                
+                                                                                
+*)
+
+End Solution4.
